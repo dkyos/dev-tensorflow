110349044c63d70269fedde5ca959e0bb3ebeea2
dk77.yun <dk77.yun@samsung.com>
2017-05-28 11:42:17 +0900

    add excel concat example
    Signed-off-by: dk77.yun <dk77.yun@samsung.com>

--- analysis/excel_read.py
+++ analysis/excel_read.py
@@ -7,11 +7,34 @@ import matplotlib as plt
 import pandas as pd 
 
 print ("===============")
-table1 = pd.read_excel("excel_test_file.xlsx", sheetname = 'tab11')
+table1 = pd.read_excel("excel_test_file.xlsx", sheetname = 'sheet1', header=3)
 print (table1)
 
 print ("===============")
-table2 = pd.read_excel("excel_test_file.xlsx", sheetname = 'tab11', header=3)
+table2 = pd.read_excel("excel_test_file.xlsx", sheetname = 'sheet2', header=3)
+print (table2)
+
+print ("===============")
+table3_concat = pd.concat([table1, table2], ignore_index=False) 
+print (table3_concat)
+
+
+# ********************************************
+# -- 엑셀 파일 쓰기(to_excel)
+#    [xlsxwriter, xlrd] 패키지가 설치되어 있어야 함
+# ********************************************
+excelOutPath = "format_new.xlsx"    # .xls는 안되더라?
+writer = pd.ExcelWriter(excelOutPath, engine="xlsxwriter")
+
+table3_concat.to_excel(writer, sheet_name="Sheetall", index=True)
+writer.save()
+
+print ("===============")
+table1 = pd.read_excel("excel_test_file.xlsx", sheetname = 'sheet1')
+print (table1)
+
+print ("===============")
+table2 = pd.read_excel("excel_test_file.xlsx", sheetname = 'sheet1', header=3)
 print (table2)
 print ("--------")
 print (table2.index)
