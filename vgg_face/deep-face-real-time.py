a48c1af25cc19597f40dc53cd5c7cb22ef4bad40
dk77.yun <dk77.yun@samsung.com>
2018-10-27 15:42:06 +0900

    update
    Signed-off-by: dk77.yun <dk77.yun@samsung.com>

--- vgg_face/deep-face-real-time.py
+++ vgg_face/deep-face-real-time.py
@@ -30,58 +30,58 @@ def preprocess_image(image_path):
     return img
 
 def loadVggFaceModel():
-	model = Sequential()
-	model.add(ZeroPadding2D((1,1),input_shape=(224,224, 3)))
-	model.add(Convolution2D(64, (3, 3), activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(64, (3, 3), activation='relu'))
-	model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(224,224, 3)))
+    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
 
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(128, (3, 3), activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(128, (3, 3), activation='relu'))
-	model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
 
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(256, (3, 3), activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(256, (3, 3), activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(256, (3, 3), activation='relu'))
-	model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
 
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, (3, 3), activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, (3, 3), activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, (3, 3), activation='relu'))
-	model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
 
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, (3, 3), activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, (3, 3), activation='relu'))
-	model.add(ZeroPadding2D((1,1)))
-	model.add(Convolution2D(512, (3, 3), activation='relu'))
-	model.add(MaxPooling2D((2,2), strides=(2,2)))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
 
-	model.add(Convolution2D(4096, (7, 7), activation='relu'))
-	model.add(Dropout(0.5))
-	model.add(Convolution2D(4096, (1, 1), activation='relu'))
-	model.add(Dropout(0.5))
-	model.add(Convolution2D(2622, (1, 1)))
-	model.add(Flatten())
-	model.add(Activation('softmax'))
-	
-	#you can download pretrained weights from https://drive.google.com/file/d/1CPSeum3HpopfomUEK1gybeuIVoeJT_Eo/view?usp=sharing
-	from keras.models import model_from_json
-	model.load_weights('./vgg_face_weights.h5')
-	
-	vgg_face_descriptor = Model(inputs=model.layers[0].input, outputs=model.layers[-2].output)
-	
-	return vgg_face_descriptor
+    model.add(Convolution2D(4096, (7, 7), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(4096, (1, 1), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(2622, (1, 1)))
+    model.add(Flatten())
+    model.add(Activation('softmax'))
+    
+    #you can download pretrained weights from https://drive.google.com/file/d/1CPSeum3HpopfomUEK1gybeuIVoeJT_Eo/view?usp=sharing
+    from keras.models import model_from_json
+    model.load_weights('./vgg_face_weights.h5')
+    
+    vgg_face_descriptor = Model(inputs=model.layers[0].input, outputs=model.layers[-2].output)
+    
+    return vgg_face_descriptor
 
 model = loadVggFaceModel()
 
@@ -93,9 +93,17 @@ employee_pictures = "./database/"
 employees = dict()
 
 for file in listdir(employee_pictures):
-	employee, extension = file.split(".")
-	employees[employee] = model.predict(preprocess_image('./database/%s.jpg' % (employee)))[0,:]
-	
+    employee, extension = file.split(".")
+
+    print(employee + " => " + extension)
+
+    if extension.lower() == "png":
+        employees[employee] = model.predict(preprocess_image('./database/%s.png' % (employee)))[0,:]
+    elif extension.lower() == "jpg":
+        employees[employee] = model.predict(preprocess_image('./database/%s.jpg' % (employee)))[0,:]
+    else:
+        print("Not support extension")
+    
 print("employee representations retrieved successfully")
 
 def findCosineSimilarity(source_representation, test_representation):
@@ -110,47 +118,48 @@ cap = cv2.VideoCapture(0) #webcam
 #cap = cv2.VideoCapture('C:/Users/IS96273/Desktop/zuckerberg.mp4') #video
 
 while(True):
-	ret, img = cap.read()
-	#img = cv2.resize(img, (640, 360))
-	faces = face_cascade.detectMultiScale(img, 1.3, 5)
-	
-	for (x,y,w,h) in faces:
-		if w > 130: 
-			#cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2) #draw rectangle to main image
-			
-			detected_face = img[int(y):int(y+h), int(x):int(x+w)] #crop detected face
-			detected_face = cv2.resize(detected_face, (224, 224)) #resize to 224x224
-			
-			img_pixels = image.img_to_array(detected_face)
-			img_pixels = np.expand_dims(img_pixels, axis = 0)
-			img_pixels /= 255
-			
-			captured_representation = model.predict(img_pixels)[0,:]
-			
-			found = 0
-			for i in employees:
-				employee_name = i
-				representation = employees[i]
-				
-				similarity = findCosineSimilarity(representation, captured_representation)
-				if(similarity < 0.30):
-					cv2.putText(img, employee_name, (int(x+w+15), int(y-12)), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
-					
-					found = 1
-					break
-					
-			#connect face and text
-			cv2.line(img,(int((x+x+w)/2),y+15),(x+w,y-20),color,1)
-			cv2.line(img,(x+w,y-20),(x+w+10,y-20),color,1)
-		
-			if(found == 0): #if found image is not in employee database
-				cv2.putText(img, 'unknown', (int(x+w+15), int(y-12)), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
-	
-	cv2.imshow('img',img)
-	
-	if cv2.waitKey(1) & 0xFF == ord('q'): #press q to quit
-		break
-	
-#kill open cv things		
+    ret, img = cap.read()
+    #img = cv2.resize(img, (640, 360))
+    faces = face_cascade.detectMultiScale(img, 1.3, 5)
+    
+    for (x,y,w,h) in faces:
+        if w > 130: 
+            #cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2) #draw rectangle to main image
+            
+            detected_face = img[int(y):int(y+h), int(x):int(x+w)] #crop detected face
+            detected_face = cv2.resize(detected_face, (224, 224)) #resize to 224x224
+            
+            img_pixels = image.img_to_array(detected_face)
+            img_pixels = np.expand_dims(img_pixels, axis = 0)
+            img_pixels /= 255
+            
+            captured_representation = model.predict(img_pixels)[0,:]
+            
+            found = 0
+            for i in employees:
+                employee_name = i
+                representation = employees[i]
+                
+                similarity = findCosineSimilarity(representation, captured_representation)
+                if(similarity < 0.30):
+                    cv2.putText(img, employee_name, (int(x+w+15), int(y-12)), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+                    found = 1
+                    print("Found => [%s]" % employee_name)
+                    break
+                    
+            #connect face and text
+            cv2.line(img,(int((x+x+w)/2),y+15),(x+w,y-20),color,1)
+            cv2.line(img,(x+w,y-20),(x+w+10,y-20),color,1)
+        
+            if(found == 0): #if found image is not in employee database
+                cv2.putText(img, 'unknown', (int(x+w+15), int(y-12)), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+                print("Not Found => [Unknown]")
+    
+    cv2.imshow('img',img)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'): #press q to quit
+        break
+    
+#kill open cv things        
 cap.release()
 cv2.destroyAllWindows()
